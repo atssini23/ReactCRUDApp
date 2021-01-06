d44f303d5b4d584f0d4292b24fbe92f340318b96
Atssini Cabrera <atssini23@yahoo.com>
2021-01-05 16:46:41 -0800

    form update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "React Simple CRUD Application",
+      act: 0,
+      index: ""
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <h2>{this.state.title}</h2>
+        <form></form>
+      </div>
+    );
+  }
 }
+
+export default App;
